ab1dba5cc15c07507ccf22edf0f1c8f3796d4087
Trọng Nguyễn <github@asakiyuki.com>
2025-08-04 18:15:24 +0700

    add neocord.lua

--- system/home/users/asalinux/programs/nixvim/plugins/extraPlugins.nix
+++ system/home/users/asalinux/programs/nixvim/plugins/extraPlugins.nix
@@ -1,9 +1,16 @@
 { pkgs, ... }:
 let
-    plugins = pkgs.vimPlugins;
+    EXTRA_PLUGINS = [
+        ./neocord.nix
+    ];
+
+    LUA = [
+        ./lua/neocord.lua
+    ];
 in
 {
-    programs.nixvim.extraPlugins = [
-        (import ./neocord.nix { inherit plugins; })
-    ];
+    programs.nixvim = {
+        extraPlugins = map (path: import path { plugins = pkgs.vimPlugins; }) EXTRA_PLUGINS;
+        extraConfigLua = builtins.concatStringsSep "\n" (map builtins.readFile LUA);
+    };
 }
