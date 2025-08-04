314e1091c6bcdea65474a4d81c69391e6b8b49c6
Trọng Nguyễn <github@asakiyuki.com>
2025-08-04 16:39:32 +0700

    rebase programs folder

--- system/home/users/asalinux/programs.nix
+++ system/home/users/asalinux/programs.nix
@@ -3,7 +3,7 @@
     programs = {
         tmux = import ./programs/tmux.nix { inherit pkgs; };
         vscode = import ./programs/vscode.nix { inherit pkgs; };
-        nixvim = import ./programs/nixvim.nix { inherit pkgs; };
+        nixvim = import ./programs/nixvim/app.nix { inherit pkgs; };
         
         ghostty = import ./programs/ghostty.nix {};
         kitty = import ./programs/kitty.nix {};
