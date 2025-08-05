dc7a7d1ad4d24f9e2cfb65637185d5639803c9f5
Trọng Nguyễn <github@asakiyuki.com>
2025-08-05 22:41:51 +0700

    treesitter

--- system/home/users/asalinux/programs/nixvim/plugins/plugins.nix
+++ system/home/users/asalinux/programs/nixvim/plugins/plugins.nix
@@ -4,5 +4,6 @@
         neo-tree = import ./neotree.nix {};
         web-devicons = import ./web-devicons.nix {};
         which-key = import ./which-key.nix {};
+        treesitter = import ./treesitter.nix {};
     };
 }
