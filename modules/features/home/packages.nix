c2f195156786d03dbb059bc2c7c5fb02cad6316c
Asaki Yuki <vantrong2007vn@gmail.com>
2026-04-10 00:57:37 +0700

    idk

--- modules/features/home/packages.nix
+++ modules/features/home/packages.nix
@@ -8,6 +8,7 @@
   home.packages = with pkgs; [
     hyprshot
     antigravity
+    prismlauncher
 
     (lib.mkIf osconfig.device.programs.cider-2.enable cider-2)
   ];
--- modules/programs/hyprland/settings/input.nix
+++ modules/programs/hyprland/settings/input.nix
@@ -1,6 +1,6 @@
 { ... }: {
     wayland.windowManager.hyprland.settings = {
-        "$LAPTOP_TOUCHPAD_ENABLE" = false;
+        "$LAPTOP_TOUCHPAD_ENABLE" = true;
 
         device = {
             name = "gxtp5100:00-27c6:01e0-touchpad";
