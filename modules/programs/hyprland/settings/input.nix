9dd9a7c09fdba28d5e20530774c33d558fd4dd2c
Asaki Yuki <vantrong2007vn@gmail.com>
2026-04-09 14:31:12 +0700

    add touchpad

--- modules/programs/hyprland/settings/input.nix
+++ modules/programs/hyprland/settings/input.nix
@@ -1,5 +1,13 @@
 { ... }: {
-    wayland.windowManager.hyprland.settings.input = {
-        touchpad.natural_scroll = true;
+    wayland.windowManager.hyprland.settings = {
+        "$LAPTOP_TOUCHPAD_ENABLE" = false;
+
+        device = {
+            name = "gxtp5100:00-27c6:01e0-touchpad";
+            enabled = "$LAPTOP_TOUCHPAD_ENABLE";
+            natural_scroll = true;
+        };
+
+        input.touchpad.natural_scroll = true;
     };
 }
