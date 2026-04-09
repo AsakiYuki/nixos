a645e356700b7d5cfb5bad79fcd94971e7a1324c
Asaki Yuki <vantrong2007vn@gmail.com>
2026-04-09 22:49:53 +0700

    add git config

--- modules/features/home/git.nix
+++ modules/features/home/git.nix
@@ -1,5 +1,19 @@
 { ... }: {
     programs.git = {
-        
+        enable = true;
+        settings = {
+            user = {
+                name = "Asaki Yuki";
+                email = "vantrong2007vn@gmail.com";
+            };
+
+            init = {
+                defaultBranch = "main";
+            };
+
+            # pull = {
+            #     rebase = "";
+            # };
+        };
     };
 }
