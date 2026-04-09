4e9bbad81d7caf2febc3859f7ad9ea75ac5db896
Asaki Yuki <vantrong2007vn@gmail.com>
2026-04-09 11:57:41 +0700

    add ghostty menu

--- home/asakiyuki/modules/files.nix
+++ home/asakiyuki/modules/files.nix
@@ -3,7 +3,9 @@
     home.file = {
         ".config/qt5ct/colors/Catppuccin-Mocha.conf".source = "${pkgs.catppuccin-qt5ct}/share/qt5ct/colors/catppuccin-mocha-sapphire.conf";
         ".config/qt6ct/colors/Catppuccin-Mocha.conf".source = "${pkgs.catppuccin-qt5ct}/share/qt6ct/colors/catppuccin-mocha-sapphire.conf";
+
         ".local/share/kio/servicemenus/open-with-code.desktop".source = ../../../configs/services-menu/open-with-code.desktop;
+        ".local/share/kio/servicemenus/open-ghostty-here.desktop".source = ../../../configs/services-menu/open-ghostty-here.desktop;
     };
 
     xdg.configFile = {
