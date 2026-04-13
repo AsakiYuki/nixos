1dc8846fb1d286b432692f17189a567d2f744ae5
Asaki Yuki <vantrong2007vn@gmail.com>
2026-04-13 20:34:08 +0700

    dvvl

--- home/asakiyuki/modules/files.nix
+++ home/asakiyuki/modules/files.nix
@@ -28,26 +28,26 @@
     ))
   ];
 
-  xdg.configFile.kdeglobals.text =
-    if (osconfig.device.wm.hyprland.enable || osconfig.device.wm.niri.enable) then
+  xdg.configFile.kdeglobals = lib.mkIf (osconfig.device.wm.hyprland.enable || osconfig.device.wm.niri.enable) {
+    text = (
       builtins.readFile (
-        (pkgs.catppuccin-kde.override {
-          flavour = [ "mocha" ];
-          accents = [ "sapphire" ];
-        })
-        + "/share/color-schemes/CatppuccinMochaSapphire.colors"
-      )
-      + ''
+          (pkgs.catppuccin-kde.override {
+            flavour = [ "mocha" ];
+            accents = [ "sapphire" ];
+          })
+          + "/share/color-schemes/CatppuccinMochaSapphire.colors"
+        )
+        + ''
 
-        [UiSettings]
-        ColorScheme=qt6ct
+          [UiSettings]
+          ColorScheme=qt6ct
 
-        [General]
-        TerminalApplication=${osconfig.device.programs.terminal.name}
+          [General]
+          TerminalApplication=${osconfig.device.programs.terminal.name}
 
-        [Icons]
-        Theme=Papirus
-      ''
-    else
-      "";
+          [Icons]
+          Theme=Papirus
+        ''
+    )
+  };
 }
