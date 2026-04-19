5900ceac82c0b4ac69b3ec62e750fbac6a785fde
Asaki Yuki <vantrong2007vn@gmail.com>
2026-04-20 04:41:02 +0700

    idk

--- modules/features/system/forgejo.nix
+++ modules/features/system/forgejo.nix
@@ -2,10 +2,10 @@
   services.forgejo = {
     enable = true;
     database.type = "mysql";
-    service.DISABLE_REGISTRATION = true;
     settings.server = {
       HTTP_PORT = 21350;
       ROOT_URL = "https://git.asakiyuki.com/";
+      service.DISABLE_REGISTRATION = true;
     };
   };
 }
