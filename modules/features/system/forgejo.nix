d2065f7c65cad5d1f6d3313c60c7956387fe6202
Asaki Yuki <vantrong2007vn@gmail.com>
2026-04-25 10:06:54 +0700

    add forgejo theme

--- modules/features/system/forgejo.nix
+++ modules/features/system/forgejo.nix
@@ -1,7 +1,32 @@
-{...}: {
+{
+  config,
+  pkgs,
+  ...
+}: let
+  catppuccinGitea = pkgs.stdenv.mkDerivation {
+    name = "catppuccin-gitea";
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/catppuccin/gitea/releases/download/v1.0.2/catppuccin-gitea.tar.gz";
+      sha256 = "sha256-rZHLORwLUfIFcB6K9yhrzr+UwdPNQVSadsw6rg8Q7gs=";
+    };
+
+    unpackPhase = "tar -xzf $src";
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r * $out/
+    '';
+  };
+in {
   services.forgejo = {
     enable = true;
     database.type = "mysql";
+    customDir = pkgs.runCommand "forgejo-custom" {} ''
+      mkdir -p $out/public/assets/css
+      cp -r ${catppuccinGitea}/* $out/public/assets/css/
+    '';
+
     settings = {
       service.DISABLE_REGISTRATION = true;
       server = {
