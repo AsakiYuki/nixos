85bd2f80a8b4141c98c288032c3674ed481a79d1
Asaki Yuki <vantrong2007vn@gmail.com>
2026-04-26 17:46:19 +0700

    change tmux prefix keybind

--- modules/features/home/tmux.nix
+++ modules/features/home/tmux.nix
@@ -62,7 +62,7 @@ in {
     enable = true;
     keyMode = "vi";
     disableConfirmationPrompt = true;
-    prefix = "d-a";
+    prefix = "c-b";
 
     extraConfig = TOP_PANEL + OPTIONS + BINDS + EXTRA_CONFIG;
 
