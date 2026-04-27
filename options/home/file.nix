9e1e6a08050411c4dda8499230d17e5642de91bd
Asaki Yuki <vantrong2007vn@gmail.com>
2026-04-27 12:37:53 +0700

    change files to options

--- options/home/file.nix
+++ options/home/file.nix
@@ -1,4 +1,32 @@
-{lib, ...}: {
+{
+  lib,
+  config,
+  ...
+}: {
+  home.file = lib.mkMerge [
+    (builtins.mapAttrs (_: path: {
+        source = path;
+      })
+      config.files.source)
+
+    (builtins.mapAttrs (_: path: {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/${path}";
+      })
+      config.files.symlink)
+
+    (lib.mkMerge (
+      builtins.map (name: lib.setAttrByPath [name "force"] true) (
+        builtins.attrNames config.files.force
+      )
+    ))
+
+    (lib.mkMerge (
+      builtins.map (name: lib.setAttrByPath [name "force"] (lib.mkForce true)) (
+        builtins.attrNames config.files.mkForce
+      )
+    ))
+  ];
+
   options.files = {
     symlink = lib.mkOption {
       default = {};
--- users/asakiyuki/files.nix
+++ users/asakiyuki/files.nix
@@ -2,33 +2,8 @@
   pkgs,
   lib,
   osconfig,
-  config,
   ...
 }: {
-  home.file = lib.mkMerge [
-    (builtins.mapAttrs (_: path: {
-        source = path;
-      })
-      config.files.source)
-
-    (builtins.mapAttrs (_: path: {
-        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/${path}";
-      })
-      config.files.symlink)
-
-    (lib.mkMerge (
-      builtins.map (name: lib.setAttrByPath [name "force"] true) (
-        builtins.attrNames config.files.force
-      )
-    ))
-
-    (lib.mkMerge (
-      builtins.map (name: lib.setAttrByPath [name "force"] (lib.mkForce true)) (
-        builtins.attrNames config.files.mkForce
-      )
-    ))
-  ];
-
   xdg.configFile.kdeglobals =
     lib.mkIf (osconfig.device.wm.hyprland.enable || osconfig.device.wm.niri.enable)
     {
