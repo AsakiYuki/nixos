69a921659a4dfc60ba7dab39966d4fe4cc5eec6c
Asaki Yuki <vantrong2007vn@gmail.com>
2026-05-07 22:07:35 +0700

    f-

--- modules/features/home/millennium.nix
+++ modules/features/home/millennium.nix
@@ -3,17 +3,23 @@
   lib,
   ...
 }: {
-  programs.steam.millennium = lib.mkIf osconfig.programs.steam.millennium.enable {
-    plugins = {
-      extendium.enable = true;
-      steamhunter-plugin.enable = true;
-      aacla.enable = true;
-      steam-non-steam-playtimes.enable = true;
-      steam-size-on-disk.enable = true;
-      valve-internal-enabler.enable = true;
-    };
-    themes = {
-      bluearchivesteamskin.enable = true;
-    };
-  };
+  # programs.steam.millennium = lib.mkIf osconfig.programs.steam.millennium.enable {
+  #   quickcss = ''
+  #     .qP17eBPXkfezFfexZ4hC3 {
+  #       display: none;
+  #     }
+  #   '';
+
+  #   plugins = {
+  #     extendium.enable = true;
+  #     hltb-millennium-plugin.enable = true;
+  #     steamhunter-plugin.enable = true;
+  #     steam-size-on-disk.enable = true;
+  #     steam-non-steam-playtimes.enable = true;
+  #   };
+
+  #   themes = {
+  #     no-more-whats-new.enable = true;
+  #   };
+  # };
 }
