3b52e87c4b13c9eee8472559819513b551fafd06
Asaki Yuki <vantrong2007vn@gmail.com>
2026-05-08 17:23:41 +0700

    refactor keybind array

--- modules/programs/hyprland/settings/key-bind.nix
+++ modules/programs/hyprland/settings/key-bind.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   wayland.windowManager.hyprland.settings = {
-    bind =
+    bind = builtins.concatLists [
       [
         "CTRL ALT, T, exec, ${osconfig.device.programs.terminal.name}"
 
@@ -32,38 +32,43 @@
         ",XF86MonBrightnessDown, exec, brightnessctl s 5%-"
         ",XF86MonBrightnessUp, exec, brightnessctl s +5%"
       ]
-      ++ builtins.concatLists (
-        builtins.map (item: let
-          at = builtins.elemAt item;
-          key = at 0;
-          keyShort = at 1;
-          vimKey = at 2;
-          vec3 = at 3;
-        in [
-          "SUPER CTRL, ${key}, movewindow, ${keyShort}"
-          "SUPER, ${key}, movefocus, ${keyShort}"
-          "SUPER, ${vimKey}, movefocus, ${keyShort}"
-          "SUPER CTRL, ${vimKey}, movewindow, ${keyShort}"
-          "SUPER SHIFT, ${key}, resizeactive, ${vec3}"
-          "SUPER SHIFT, ${vimKey}, resizeactive, ${vec3}"
-        ]) [
-          ["LEFT" "l" "h" "-25 0"]
-          ["RIGHT" "r" "l" "25 0"]
-          ["UP" "u" "k" "0 -25"]
-          ["DOWN" "d" "j" "0 25"]
-        ]
+      (
+        builtins.concatLists (
+          builtins.map (item: let
+            at = builtins.elemAt item;
+            key = at 0;
+            keyShort = at 1;
+            vimKey = at 2;
+            vec3 = at 3;
+          in [
+            "SUPER CTRL, ${key}, movewindow, ${keyShort}"
+            "SUPER, ${key}, movefocus, ${keyShort}"
+            "SUPER, ${vimKey}, movefocus, ${keyShort}"
+            "SUPER CTRL, ${vimKey}, movewindow, ${keyShort}"
+            "SUPER SHIFT, ${key}, resizeactive, ${vec3}"
+            "SUPER SHIFT, ${vimKey}, resizeactive, ${vec3}"
+          ]) [
+            ["LEFT" "l" "h" "-25 0"]
+            ["RIGHT" "r" "l" "25 0"]
+            ["UP" "u" "k" "0 -25"]
+            ["DOWN" "d" "j" "0 25"]
+          ]
+        )
       )
-      ++ builtins.concatLists (
-        builtins.genList (i: [
-          "SUPER, ${toString (i + 1)}, workspace,${toString (i + 1)}"
-          "SUPER SHIFT, ${toString (i + 1)}, movetoworkspace,${toString (i + 1)}"
-        ])
-        9
+      (
+        builtins.concatLists (
+          builtins.genList (i: [
+            "SUPER, ${toString (i + 1)}, workspace,${toString (i + 1)}"
+            "SUPER SHIFT, ${toString (i + 1)}, movetoworkspace,${toString (i + 1)}"
+          ])
+          9
+        )
       )
-      ++ [
+      [
         "SUPER, 0, workspace, 10"
         "SUPER SHIFT, 0, movetoworkspace, 10"
-      ];
+      ]
+    ];
 
     bindi = [
       ", Caps_Lock, global, asakiyuki:capslock"
