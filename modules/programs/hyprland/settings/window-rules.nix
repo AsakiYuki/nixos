00a4698abfb4f18aa9024a62f5ad3bdaccf6c72d
Asaki Yuki <vantrong2007vn@gmail.com>
2026-05-08 14:21:58 +0700

    fix hyprland.conf

--- modules/programs/hyprland/settings/window-rules.nix
+++ modules/programs/hyprland/settings/window-rules.nix
@@ -1,22 +1,11 @@
 {...}: {
   wayland.windowManager.hyprland.settings.windowrule = [
-    # "float, class:(clipse)"
-    # "size 622 652, class:(clipse)"
-    # "stayfocused, class:(clipse)"
+    "match:class ^clipse$, float on, size 622 652, suppress_event activatefocus"
 
-    # "suppressevent maximize, class:.*"
-    # "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
-
-    # "opacity 0.0 override, class:^(xwaylandvideobridge)$"
-    # "noanim, class:^(xwaylandvideobridge)$"
-    # "noinitialfocus, class:^(xwaylandvideobridge)$"
-    # "maxsize 1 1, class:^(xwaylandvideobridge)$"
-    # "noblur, class:^(xwaylandvideobridge)$"
-    # "nofocus, class:^(xwaylandvideobridge)$"
-  ];
-  wayland.windowManager.hyprland.settings.windowrulev2 = [
-    # "tile, class:^(Chromium)$"
-    # "fullscreen, initialClass:^(wlroots|Waydroid)$, initialTitle:^(wlroots - WL-1|Waydroid)$"
-    # "noborder, initialClass:^(wlroots|Waydroid)$, initialTitle:^(wlroots - WL-1|Waydroid)$"
+    "match:class ^.*$, suppress_event maximize"
+    "match:class ^$, match:title ^$, match:xwayland true, match:float true, match:fullscreen false, match:pin false, no_focus on"
+    "match:class ^xwaylandvideobridge$, opacity 0 override, no_anim on, no_initial_focus on, max_size 1 1, no_blur on, no_focus on"
+    "match:class ^Chromium$, tile on"
+    "match:initial_class ^(wlroots|Waydroid)$, match:initial_title ^(wlroots - WL-1|Waydroid)$, fullscreen on, border_size 0"
   ];
 }
