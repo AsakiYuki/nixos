b290a7b5270a81f19c3e65f3490061718b53731a
Asaki Yuki <vantrong2007vn@gmail.com>
2026-05-12 22:49:54 +0700

    idk

--- modules/features/system/stalwart.nix
+++ modules/features/system/stalwart.nix
@@ -8,26 +8,20 @@
     };
 
     settings = {
-      # ĐỊNH NGHĨA STORAGE GỐC KHỚP VỚI TÊN "db" CỦA MODULE
-      storage = {
-        data.db = {
-          dsn = "%{file:/run/credentials/stalwart-mail.service/db-dsn}%";
-          type = "sql";
-        };
-        directory = "db";
-        blob = "db";
-        lookup = "db";
-        fts = "db";
+      "store.db" = {
+        dsn = "%{file:/run/credentials/stalwart-mail.service/db-dsn}%";
+        type = "sql";
       };
+
+      storage.directory = "db";
+      storage.blob = "db";
+      storage.lookup = "db";
+      storage.fts = "db";
 
       server.hostname = "mx1.asakiyuki.com";
 
-      # LISTENER
       server.listener = {
-        smtp = {
-          bind = ["[::]:25"];
-          protocol = "smtp";
-        };
+        smtp.bind = ["[::]:25"];
         submissions = {
           bind = "[::]:465";
           protocol = "smtp";
@@ -38,10 +32,7 @@
           protocol = "imap";
           tls.implicit = true;
         };
-        management = {
-          bind = ["0.0.0.0:47291"];
-          protocol = "http";
-        };
+        management.bind = ["0.0.0.0:47291"];
       };
 
       lookup.default = {
@@ -49,8 +40,7 @@
         domain = "asakiyuki.com";
       };
 
-      # USER DIRECTORY
-      directory.sql.type = "sql";
+      "directory.sql".type = "sql";
 
       authentication.fallback-admin = {
         user = "admin";
