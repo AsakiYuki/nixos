fd8277f8ae8ad4446d8e8f3cb0a2a2a4075663bb
Asaki Yuki <vantrong2007vn@gmail.com>
2026-05-12 21:52:27 +0700

    idk

--- modules/features/system/stalwart.nix
+++ modules/features/system/stalwart.nix
@@ -1,5 +1,37 @@
 {...}: {
   services.stalwart-mail = {
     enable = true;
+    settings = {
+      hostname = "mail.asakiyuki.com";
+      tls = {
+        enable = true;
+        implicit = true;
+      };
+      listener = {
+        smtp = {
+          protocol = "smtp";
+          bind = "[::]:25";
+        };
+        submissions = {
+          bind = "[::]:465";
+          protocol = "smtp";
+          tls.implicit = true;
+        };
+        imaps = {
+          bind = "[::]:993";
+          protocol = "imap";
+          tls.implicit = true;
+        };
+        jmap = {
+          bind = "[::]:42795";
+          url = "https://mail.asakiyuki.com";
+          protocol = "http";
+        };
+        management = {
+          bind = ["127.0.0.1:42795"];
+          protocol = "http";
+        };
+      };
+    };
   };
 }
