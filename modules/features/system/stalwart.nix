51f253841b79df2689634396ef5e97189c120fca
Asaki Yuki <vantrong2007vn@gmail.com>
2026-05-12 22:39:43 +0700

    idk

--- modules/features/system/stalwart.nix
+++ modules/features/system/stalwart.nix
@@ -8,42 +8,46 @@
     };
 
     settings = {
-      "storage.data.rocksdb" = {
-        path = "/var/lib/stalwart-mail/data";
-        type = "rocksdb";
+      storage = {
+        data.rocksdb = {
+          path = "/var/lib/stalwart-mail/data";
+          type = "rocksdb";
+        };
+        directory = "rocksdb";
+        blob = "rocksdb";
+        lookup = "rocksdb";
       };
 
-      storage.directory = "rocksdb";
-      storage.blob = "rocksdb";
-      storage.lookup = "rocksdb";
+      server = {
+        hostname = "mx1.asakiyuki.com";
+        listener = {
+          smtp = {
+            bind = ["[::]:25"];
+            protocol = "smtp";
+          };
+          submissions = {
+            bind = "[::]:465";
+            protocol = "smtp";
+            tls.implicit = true;
+          };
+          imaps = {
+            bind = "[::]:993";
+            protocol = "imap";
+            tls.implicit = true;
+          };
+          management = {
+            bind = ["0.0.0.0:47291"];
+            protocol = "http";
+          };
+        };
+      };
 
-      server.hostname = "mx1.asakiyuki.com";
-
-      "server.listener.smtp" = {
-        bind = ["[::]:25"];
-        protocol = "smtp";
-      };
-      "server.listener.submissions" = {
-        bind = "[::]:465";
-        protocol = "smtp";
-        tls.implicit = true;
-      };
-      "server.listener.imaps" = {
-        bind = "[::]:993";
-        protocol = "imap";
-        tls.implicit = true;
-      };
-      "server.listener.management" = {
-        bind = ["0.0.0.0:47291"];
-        protocol = "http";
-      };
-
-      "lookup.default" = {
+      lookup.default = {
         hostname = "mx1.asakiyuki.com";
         domain = "asakiyuki.com";
       };
 
-      "directory.in-memory" = {
+      directory.in-memory = {
         type = "memory";
         principals = [
           {
@@ -55,7 +59,7 @@
         ];
       };
 
-      "authentication.fallback-admin" = {
+      authentication.fallback-admin = {
         user = "admin";
         secret = "%{file:/run/credentials/stalwart-mail.service/admin-pw}%";
       };
