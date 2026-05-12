18c82155d362b34e6bfbe86033d0eee00251b6e3
Asaki Yuki <vantrong2007vn@gmail.com>
2026-05-12 22:21:23 +0700

    idk

--- modules/services/cloudflare-dyndns.nix
+++ modules/services/cloudflare-dyndns.nix
@@ -1,13 +1,12 @@
-{ ... }:
-{
+{...}: {
   services.cloudflare-dyndns = {
     enable = true;
     apiTokenFile = "/home/asakiyuki/.secret/CLOUDFLARE_TOKEN_KEY";
     frequency = ":0/5";
-    proxied = true;
+    proxied = false;
     ipv6 = false;
     ipv4 = true;
     deleteMissing = false;
-    domains = [ "ddns.asakiyuki.com" ];
+    domains = ["ddns.asakiyuki.com" "mx1.asakiyuki.com"];
   };
 }
