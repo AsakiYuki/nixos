361bb4d7f68ac7ffa0a9c0c38e6cd23d796127f1
Asaki Yuki <vantrong2007vn@gmail.com>
2026-05-12 21:53:14 +0700

    idk

--- host/server/default.nix
+++ host/server/default.nix
@@ -10,6 +10,7 @@
     (libs.root "/modules/features/system/docker.nix")
     (libs.root "/modules/features/system/packages.nix")
     (libs.root "/modules/features/system/nix-dl.nix")
+    (libs.root "/modules/features/system/stalwart.nix")
 
     (libs.root "/users/asakiyuki/configuration.nix")
     (libs.root "/users/junko/configuration.nix")
