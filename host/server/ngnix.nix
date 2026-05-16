65ada5a9423ebae24b30ae0345e884d8f8b92cda
Asaki Yuki <vantrong2007vn@gmail.com>
2026-05-16 10:25:16 +0700

    idk

--- host/server/ngnix.nix
+++ host/server/ngnix.nix
@@ -4,15 +4,15 @@
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
     virtualHosts = {
-      "_" = {
-        default = true;
-        onlySSL = true;
-        sslCertificate = "/var/lib/acme/dummy.crt";
-        sslCertificateKey = "/var/lib/acme/dummy.key";
-        locations."/" = {
-          return = "444";
-        };
-      };
+      # "_" = {
+      #   default = true;
+      #   onlySSL = true;
+      #   sslCertificate = "/var/lib/acme/dummy.crt";
+      #   sslCertificateKey = "/var/lib/acme/dummy.key";
+      #   locations."/" = {
+      #     return = "444";
+      #   };
+      # };
 
       "asakiyuki.com" = {
         forceSSL = true;
