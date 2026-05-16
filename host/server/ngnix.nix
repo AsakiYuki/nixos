0f5506ff97fd9d1f594b4e91a8c5ae4d962fbb5f
Asaki Yuki <vantrong2007vn@gmail.com>
2026-05-16 10:36:01 +0700

    idk

--- host/server/ngnix.nix
+++ host/server/ngnix.nix
@@ -5,22 +5,10 @@
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
     virtualHosts = {
-      "_" = {
-        default = true;
-        onlySSL = true;
-        sslCertificate = "/var/lib/acme/dummy.crt";
-        sslCertificateKey = "/var/lib/acme/dummy.key";
-        locations."/" = {
-          return = "444";
-        };
-      };
-
       "asakiyuki.com" = {
         forceSSL = true;
         enableACME = true;
-        locations."/" = {
-          return = "301 https://www.asakiyuki.com$request_uri";
-        };
+        globalRedirect = "www.asakiyuki.com";
       };
 
       "adguard.asakiyuki.com" = {
