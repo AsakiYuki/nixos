19f2d3d34331f8c259c8f42f79a7fbca7b0f8dd2
Asaki Yuki <vantrong2007vn@gmail.com>
2026-05-19 18:06:29 +0700

    idk

--- modules/features/home/fzf.nix
+++ modules/features/home/fzf.nix
@@ -1,5 +1,37 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   programs.fzf = {
     enable = true;
+    defaultCommand = null;
+    enableBashIntegration = true;
+
+    fileWidgetOptions = [
+      "--preview 'head {}'"
+    ];
+
+    historyWidgetOptions = [
+      "--sort"
+      "--exact"
+    ];
+
+    defaultOptions = [
+      "--height 40%"
+      "--border"
+    ];
+
+    # fileWidgetCommand = "fd --type f";
+    # changeDirWidgetCommand = "fd --type d";
+
+    colors = {};
+
+    tmux = lib.mkIf config.programs.tmux.enable {
+      enableShellIntegration = true;
+      shellIntegrationOptions = [
+        "-d 40%"
+      ];
+    };
   };
 }
