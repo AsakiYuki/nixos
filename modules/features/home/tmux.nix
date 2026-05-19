de1ede3a51cafefa3a7e43bb69a73a88d457ecd9
Asaki Yuki <vantrong2007vn@gmail.com>
2026-05-19 17:25:44 +0700

    idk

--- modules/features/home/tmux.nix
+++ modules/features/home/tmux.nix
@@ -9,7 +9,7 @@
     set -g pane-border-lines single
     set -g pane-border-indicators colour
 
-    set -g pane-border-format "#{E:@asa-module-icon} #{pane_index} #{E:@asa-module-text} #{pane_current_path} #[default]"
+    set -g pane-border-format "#{E:@asa-module-icon} #{pane_index} #{E:@asa-module-text} #{pane_current_command}:#{pane_current_path} #[default]"
   '';
 
   OPTIONS = ''
@@ -20,35 +20,6 @@
   BINDS = ''
     unbind r
     bind r source-file ~/.config/tmux/tmux.conf;
-  '';
-
-  BOTTOM_PANEL = ''
-    set -g status-justify "centre"
-
-    set -g window-status-format "#{E:@asa-module-icon} #I #{E:@asa-module-text} #W "
-    set -g window-status-current-format "#{E:@asa-module-icon} #I #{E:@asa-module-text} #W "
-
-    set -gg status-left "#{E:@asa-module-text} %A %d/%m/%Y #[default] "
-    set -ag status-left "#{E:@asa-module-text} %I:%M %p #[default] "
-    set -g status-left-length 100
-
-    set -g status-right "#{E:@asa-module-icon}  #{E:@asa-module-text} #S #[default] "
-    set -ag status-right "#{E:@asa-module-icon} CPU #{E:@asa-module-text} #{cpu_percentage} #[default] "
-    set -ag status-right "#{E:@asa-module-icon} RAM #{E:@asa-module-text} #{ram_percentage} "
-    set -g status-right-length 100
-
-    setw -g automatic-rename off
-  '';
-
-  CATPPUCCIN = ''
-    set -g @catppuccin_flavor 'mocha'
-    set -ogq @asa-module-icon "#[bg=#{E:@thm_sapphire},fg=#{E:@thm_surface_0}]"
-    set -ogq @asa-module-text "#[bg=#{E:@thm_surface_0},fg=#{E:@thm_fg}]"
-  '';
-
-  YANK = ''
-    set -g @yank_selection 'primary'
-    set -g @yank_selection_mouse 'primary'
   '';
 
   EXTRA_CONFIG = ''
@@ -69,15 +40,38 @@ in {
     plugins = with pkgs.tmuxPlugins; [
       {
         plugin = yank;
-        extraConfig = YANK;
+        extraConfig = ''
+          set -g @yank_selection 'primary'
+          set -g @yank_selection_mouse 'primary'
+        '';
       }
       {
         plugin = catppuccin;
-        extraConfig = CATPPUCCIN;
+        extraConfig = ''
+          set -g @catppuccin_flavor 'mocha'
+          set -ogq @asa-module-icon "#[bg=#{E:@thm_sapphire},fg=#{E:@thm_surface_0}]"
+          set -ogq @asa-module-text "#[bg=#{E:@thm_surface_0},fg=#{E:@thm_fg}]"
+        '';
       }
       {
         plugin = cpu;
-        extraConfig = BOTTOM_PANEL;
+        extraConfig = ''
+          set -g status-justify "centre"
+
+          set -g window-status-format "#{E:@asa-module-icon} #I #{E:@asa-module-text} #W "
+          set -g window-status-current-format "#{E:@asa-module-icon} #I #{E:@asa-module-text} #W "
+
+          set -gg status-left "#{E:@asa-module-text} %A %d/%m/%Y #[default] "
+          set -ag status-left "#{E:@asa-module-text} %I:%M %p #[default] "
+          set -g status-left-length 100
+
+          set -g status-right "#{E:@asa-module-icon}  #{E:@asa-module-text} #S #[default] "
+          set -ag status-right "#{E:@asa-module-icon} CPU #{E:@asa-module-text} #{cpu_percentage} #[default] "
+          set -ag status-right "#{E:@asa-module-icon} RAM #{E:@asa-module-text} #{ram_percentage} "
+          set -g status-right-length 100
+
+          setw -g automatic-rename off
+        '';
       }
     ];
   };
