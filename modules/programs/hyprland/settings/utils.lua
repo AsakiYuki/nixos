4cd3ed33d20b9e6136e15ed7cf0194c7564351ce
Asaki Yuki <vantrong2007vn@gmail.com>
2026-06-03 11:04:18 +0700

    rename inspectTable to InspectTable and data type check

--- modules/programs/hyprland/settings/utils.lua
+++ modules/programs/hyprland/settings/utils.lua
@@ -1,4 +1,6 @@
-local function inspectTable(t)
+function InspectTable(t)
+    if (type(t) ~= "table") then return "" end
+
     local outStr = ""
     for k, v in pairs(t) do
         outStr = outStr .. tostring(k) .. " = " .. tostring(v) .. "\n"
