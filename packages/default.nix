b323313a1ff2b8d532cad14fb6539717db845714
Asaki Yuki <vantrong2007vn@gmail.com>
2026-06-13 21:21:55 +0700

    add cider-2

--- packages/default.nix
+++ packages/default.nix
@@ -1,7 +1,8 @@
-{...}: {
+{pkgs, ...}: {
   bun = import ./bun.nix;
   cage-xtmapper = {version ? "0.2.0"}: import ./cage-xtmapper-${version}.nix;
   catppuccin-obs = import ./catppuccin-obs.nix;
+  cider-2 = import ./cider-2.nix {inherit pkgs;};
   cursors = {
     name ? "aemeath",
     size ? 48,
