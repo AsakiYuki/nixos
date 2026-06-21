e094c4a0699d0b97607b24c5c2355ac561655f6d
Asaki Yuki <vantrong2007vn@gmail.com>
2026-06-21 10:30:25 +0700

    idk

--- modules/features/system/cloudflare.nix
+++ modules/features/system/cloudflare.nix
@@ -2,10 +2,13 @@
   config,
   lib,
   libs,
+  pkgs,
   ...
-}: {
+}: let
+  rawScript = builtins.readFile (libs.root "/scripts/completions/cloudflare.sh");
+  finalScript = lib.replaceStrings ["@warpCli@"] ["${pkgs.cloudflare-warp}/bin/warp-cli"] rawScript;
+in {
   services.cloudflare-warp.enable = true;
   programs.bash.interactiveShellInit =
-    lib.mkIf config.services.cloudflare-warp.enable
-    (builtins.readFile (libs.root "/scripts/completions/cloudflare.sh"));
+    lib.mkIf config.services.cloudflare-warp.enable finalScript;
 }
--- scripts/completions/cloudflare.sh
+++ scripts/completions/cloudflare.sh
@@ -1,5 +1,5 @@
 if ! complete -p warp-cli &>/dev/null; then
   if systemctl is-active --quiet cloudflare-warp.service; then
-    source <(${pkgs.cloudflare-warp}/bin/warp-cli generate-completions bash) 2>/dev/null || true
+    source <(@warpCli@ generate-completions bash) 2>/dev/null || true
   fi
 fi
