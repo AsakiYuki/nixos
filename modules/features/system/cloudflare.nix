99071f1a7f28cbc7f43091f66b4278a1a71481ab
Asaki Yuki <vantrong2007vn@gmail.com>
2026-06-21 10:02:35 +0700

    add warp-cli auto completion

--- modules/features/system/cloudflare.nix
+++ modules/features/system/cloudflare.nix
@@ -1,5 +1,15 @@
-{...}: {
-  services = {
-    cloudflare-warp.enable = true;
-  };
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  services.cloudflare-warp.enable = true;
+  programs.bash.interactiveShellInit = lib.mkIf config.services.cloudflare-warp.enable ''
+    if ! complete -p warp-cli &>/dev/null; then
+      if systemctl is-active --quiet cloudflare-warp.service; then
+        source <(${pkgs.cloudflare-warp}/bin/warp-cli generate-completions bash) 2>/dev/null || true
+      fi
+    fi
+  '';
 }
