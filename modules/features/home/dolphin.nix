57090316534ddc52a33858f7aa2c655203c967d5
Asaki Yuki <vantrong2007vn@gmail.com>
2026-06-22 17:56:03 +0000

    only enable a context menu when a programs enable

--- modules/features/home/dolphin.nix
+++ modules/features/home/dolphin.nix
@@ -20,38 +20,55 @@
         };
       };
 
-      open-here = {
-        "Desktop Entry" = {
-          Type = "Service";
-          ServiceTypes = "KonqPopupMenu/Plugin";
-          MimeType = "inode/directory";
-          Actions = "RunCodeDir;RunGhosttyDir;RunNvimDir;RunAntigravityDir;";
-        };
+      open-here = lib.mergeAttrsList (let 
+        cfg = config.programs;
+      in  [
+        {
+          "Desktop Entry" = {
+            Type = "Service";
+            ServiceTypes = "KonqPopupMenu/Plugin";
+            MimeType = "inode/directory";
+            Actions = (lib.join ";" [
+              (lib.optionalString cfg.vscode.enable "RunCodeDir")
+              (lib.optionalString cfg.ghostty.enable "RunGhosttyDir")
+              (lib.optionalString cfg.nixvim.enable "RunNvimDir")
+              (lib.optionalString cfg.antigravity.enable "RunAntigravityDir")
+            ]);
+          };
+        }
 
-        "Desktop Action RunCodeDir" = {
-          Name = "Open with Code";
-          Icon = "vscode";
-          Exec = "code \"%F\"";
-        };
+        (lib.optionalAttrs cfg.vscode.enable {
+          "Desktop Action RunCodeDir" = {
+            Name = "Open with Code";
+            Icon = "vscode";
+            Exec = "code \"%F\"";
+          };
+        })
 
-        "Desktop Action RunGhosttyDir" = {
-          Name = "Open Ghostty here";
-          Icon = "com.mitchellh.ghostty";
-          Exec = "ghostty +new-window --working-directory=\"%F\"";
-        };
+        (lib.optionalAttrs cfg.ghostty.enable {
+          "Desktop Action RunGhosttyDir" = {
+            Name = "Open Ghostty here";
+            Icon = "com.mitchellh.ghostty";
+            Exec = "ghostty +new-window --working-directory=\"%F\"";
+          };
+        })
 
-        "Desktop Action RunNvimDir" = {
-          Name = "Open with Nvim";
-          Icon = "nvim";
-          Exec = "ghostty +new-window --working-directory=\"~/.config\" -e nvim .";
-        };
+        (lib.optionalAttrs cfg.nixvim.enable {
+          "Desktop Action RunNvimDir" = {
+            Name = "Open with Nvim";
+            Icon = "nvim";
+            Exec = "ghostty +new-window --working-directory=\"~/.config\" -e nvim .";
+          };
+        })
 
-        "Desktop Action RunAntigravityDir" = {
-          Name = "Open with Antigravity";
-          Icon = "antigravity";
-          Exec = "antigravity \"%F\"";
-        };
-      };
+        (lib.optionalAttrs cfg.antigravity.enable {
+          "Desktop Action RunAntigravityDir" = {
+            Name = "Open with Antigravity";
+            Icon = "antigravity";
+            Exec = "antigravity \"%F\"";
+          };
+        })
+      ]);
     };
     configs = {
       General = {
