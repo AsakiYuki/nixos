bd5f9624a88d9a53c0934faa6ad56a4fa2188a2a
Asaki Yuki <vantrong2007vn@gmail.com>
2026-06-24 16:47:27 +0700

    fix shell conflict

--- devices/home-server/configuration.nix
+++ devices/home-server/configuration.nix
@@ -1,8 +1,15 @@
-{libs, ...}: {
+{
+  libs,
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     ./hardware-configuration.nix
     ./network.nix
     ./boot.nix
     (libs.root "/host/server/default.nix")
   ];
+
+  users.users.root.shell = lib.mkForce pkgs.bashInteractive;
 }
