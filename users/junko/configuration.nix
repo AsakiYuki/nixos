51356af867cb1f187f6852cc2b1871d12b73a773
Asaki Yuki <vantrong2007vn@gmail.com>
2026-06-24 16:48:42 +0700

    fix user junko

--- users/junko/configuration.nix
+++ users/junko/configuration.nix
@@ -4,5 +4,8 @@
   ...
 }:
 libs.mkUsers config {
-  root.openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQMWoMdcMNT4ceERAWWVcI7kS6x42Z8BjyYyvJ+rh/B vantrong2007vn@gmail.com"];
+  root = {
+    openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQMWoMdcMNT4ceERAWWVcI7kS6x42Z8BjyYyvJ+rh/B vantrong2007vn@gmail.com"];
+    isNormalUser = true;
+  };
 }
