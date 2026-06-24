8af64cd6f1bfb62498a365c967f5bfc0864612eb
Asaki Yuki <vantrong2007vn@gmail.com>
2026-06-24 23:56:31 +0700

    add mkUsers file

--- libs/default.nix
+++ libs/default.nix
@@ -1,41 +1,4 @@
-{
-  lib,
-  inputs,
-  state-version,
-  unstable,
-  custom,
-  libs,
-  ...
-}: {
+{...} @ args: {
   root = path: ../. + path;
-  mkUsers = osconfig: users:
-    lib.mergeAttrsList (map ({
-      name,
-      value,
-    }: let
-      root = value.root or {};
-      home = value.home or {};
-    in {
-      users.users.${name} = lib.mergeAttrs {isNormalUser = true;} root;
-      home-manager.users.${name} = lib.mergeAttrs home {
-        _module.args = lib.mergeAttrs {
-          inherit inputs unstable custom libs osconfig;
-        } (lib.attrByPath ["_module" "args"] {} home);
-
-        imports = lib.concatLists [
-          [
-            ../options/home/default.nix
-            inputs.nixvim.homeModules.nixvim
-            inputs.nixcord.homeModules.nixcord
-            inputs.niri.homeModules.niri
-          ]
-          (home.imports or [])
-        ];
-
-        home = {
-          username = name;
-          stateVersion = state-version;
-        };
-      };
-    }) (lib.attrsToList users));
+  mkUsers = import ./mkUsers.nix args;
 }
