c751a8bc6cda379d27a0e0a9b572e48e2fa63e56
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-01 21:02:01 +0700

    add femboi

--- devices/home-server/services/nginx.nix
+++ devices/home-server/services/nginx.nix
@@ -50,12 +50,6 @@
         enableACME = true;
         locations."/" = {
           proxyPass = "http://localhost:39212";
-          extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-          '';
         };
       };
 
