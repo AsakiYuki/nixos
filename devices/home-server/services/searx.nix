257c512c336655c3cafa5e911f5458a5cd73b8dc
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-01 20:33:12 +0700

    add server settings to searx

--- devices/home-server/services/searx.nix
+++ devices/home-server/services/searx.nix
@@ -1,3 +1,13 @@
 {...}: {
-  services.searx = {};
+  services.searx = {
+    enable = true;
+    # domain = "search.asakiyuki.com";
+    settings = {
+      server = {
+        port = 65530;
+        bind_address = "0.0.0.0";
+        secret_key = "$SEARX_SECRET_KEY";
+      };
+    };
+  };
 }
