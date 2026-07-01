3263a41f00900ae1a7bf6a9210cdb2dd1a60ad65
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-01 21:13:14 +0700

    idk

--- devices/home-server/security/acme.nix
+++ devices/home-server/security/acme.nix
@@ -1,7 +1,9 @@
 {...}: {
   security.acme = {
     acceptTerms = true;
-    defaults.email = "me@asakiyuki.com";
-    defaults.server = "https://acme-v02.api.letsencrypt.org/directory";
+    defaults = {
+      email = "me@asakiyuki.com";
+      server = "https://acme-v02.api.letsencrypt.org/directory";
+    };
   };
 }
