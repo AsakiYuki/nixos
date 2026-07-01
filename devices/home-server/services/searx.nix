a45a5cf874fbf6786527741c071a500a8cf59dc7
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-01 20:46:33 +0700

    add secret key to searx

--- devices/home-server/services/searx.nix
+++ devices/home-server/services/searx.nix
@@ -6,7 +6,7 @@
       server = {
         port = 39212;
         bind_address = "0.0.0.0";
-        secret_key = "$SEARX_SECRET_KEY";
+        secret_key = "4e79f3fb5cb25b211a9676f266de4396bb18298373552396f1ce6717e19573b7";
       };
     };
   };
