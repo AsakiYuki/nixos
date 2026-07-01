d19c78954a35be8e894360739dddc867509ea701
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-01 20:38:57 +0700

    open firewall when searx enable

--- devices/home-server/services/searx.nix
+++ devices/home-server/services/searx.nix
@@ -4,7 +4,7 @@
     # domain = "search.asakiyuki.com";
     settings = {
       server = {
-        port = 65530;
+        port = 39212;
         bind_address = "0.0.0.0";
         secret_key = "$SEARX_SECRET_KEY";
       };
