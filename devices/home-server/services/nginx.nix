41a5543103b1e4374c85a1682e96ba4f5b83e13d
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-02 12:30:03 +0700

    idk

--- devices/home-server/services/nginx.nix
+++ devices/home-server/services/nginx.nix
@@ -59,7 +59,6 @@
             proxy_set_header Host $host;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header X-Forwarded-Proto $scheme;
-            auth_basic "Please login to use SearXNG";
           '';
         };
       };
