8b68aba9a889373f0dd295a42284bd0819279e0c
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-05 03:17:43 +0700

    move niri to programs

--- modules/features/home/default.nix
+++ modules/features/home/default.nix
@@ -1,7 +1,6 @@
 {...}: {
   imports = [
     ./programs
-    ./niri.nix
     ./obs-studio.nix
     ./theme.nix
     ./xdg.nix
--- modules/features/home/programs/default.nix
+++ modules/features/home/programs/default.nix
@@ -4,5 +4,6 @@
     ./browsers
     ./terminals
     ./dolphin.nix
+    ./niri.nix
   ];
 }
