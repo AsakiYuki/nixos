e09fde87879d85e854f070bf502e4148c1f18387
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-04 22:26:52 +0700

    idk

--- modules/programs/hyprland/settings/events.lua
+++ modules/programs/hyprland/settings/events.lua
@@ -1,15 +1,16 @@
-hl.on("window.open",
-    ---@param window HL.Window
-    function (window)
-        local monitor = window.monitor
-        if (monitor == nil) then return end
-        if (window.fullscreen or window.over_fullscreen) then return end
-        if (window.size.x == monitor.size.width and window.size.y == monitor.size.height) then
-            hl.dsp.window.fullscreen({
-                mode = "fullscreen",
-                action = "set",
-                window = window,
-            })
-        end
+---@param window HL.Window
+local function setFullscreen (window)
+    local monitor = window.monitor
+    if (monitor == nil) then return end
+    if (window.fullscreen == 2) then return end
+    if (window.size.x == monitor.size.width and window.size.y == monitor.size.height) then
+
+        hl.dispatch(hl.dsp.window.fullscreen({
+            mode = "fullscreen",
+            action = "set",
+            window = window,
+        }))
     end
-)
+end
+
+hl.on("window.active", setFullscreen)
