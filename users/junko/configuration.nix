2426b9b99b057a01b3a8570433827c37d8f30a56
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-09 02:32:12 +0700

    idk

--- users/junko/configuration.nix
+++ users/junko/configuration.nix
@@ -6,10 +6,12 @@
 }:
 libs.mkUsers config {
   junko = {
-    root.openssh.authorizedKeys.keys =
-      (lib.attrByPath ["users" "users" "asakiyuki" "openssh" "authorizedKeys" "keys"] [] config)
-      ++ (libs.readRootFiles [
-        "/assets/public/ssh/junko/windows/hp-victus.pub"
-      ]);
+    root = {
+      openssh.authorizedKeys.keys =
+        (lib.attrByPath ["users" "users" "asakiyuki" "openssh" "authorizedKeys" "keys"] [] config)
+        ++ (libs.readRootFiles [
+          "/assets/public/ssh/junko/windows/hp-victus.pub"
+        ]);
+    };
   };
 }
