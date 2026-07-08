742da8545d681986b742d2c5950c67308ad5e33e
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-09 02:27:06 +0700

    add asakiyuki's ssh keys to junko

--- users/junko/configuration.nix
+++ users/junko/configuration.nix
@@ -5,9 +5,10 @@
 }:
 libs.mkUsers config {
   junko = {
-    root.openssh.authorizedKeys.keys = libs.readRootFiles [
-      "/assets/public/ssh/asakiyuki/nixos/ideapad.pub"
-      "/assets/public/ssh/junko/windows/hp-victus.pub"
-    ];
+    root.openssh.authorizedKeys.keys =
+      (config.users.users.asakiyuki.openssh.authorizedKeys.keys)
+      ++ (libs.readRootFiles [
+        "/assets/public/ssh/junko/windows/hp-victus.pub"
+      ]);
   };
 }
