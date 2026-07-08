9d4274e79edd26fc38024a8871166554fb50f47e
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-09 02:30:30 +0700

    idk

--- users/junko/configuration.nix
+++ users/junko/configuration.nix
@@ -7,7 +7,7 @@
 libs.mkUsers config {
   junko = {
     root.openssh.authorizedKeys.keys =
-      (lib.attrByPath ["users" "asakiyuki" "openssh" "authorizedKeys" "keys"] [] config)
+      (lib.attrByPath ["users" "users" "asakiyuki" "openssh" "authorizedKeys" "keys"] [] config)
       ++ (libs.readRootFiles [
         "/assets/public/ssh/junko/windows/hp-victus.pub"
       ]);
