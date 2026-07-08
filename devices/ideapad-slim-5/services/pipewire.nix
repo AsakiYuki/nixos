4cb594ac41d05795e5cddf7945f1c9e15754004c
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-09 00:11:39 +0700

    fix(audio): add oneshot service to unmute mic on boot to fix ALSA persistence issues

--- devices/ideapad-slim-5/services/pipewire.nix
+++ devices/ideapad-slim-5/services/pipewire.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   services.pipewire.wireplumber.extraConfig = {
     "50-disable-dmic" = {
       "monitor.alsa.rules" = [
@@ -24,6 +24,21 @@
           };
         }
       ];
+    };
+  };
+
+  systemd.services.unmute-mic = {
+    description = "Unmute Analog Microphone on Boot";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "sound.target" ];
+    script = ''
+      ${pkgs.alsa-utils}/bin/amixer -c 1 set Capture cap || true
+      ${pkgs.alsa-utils}/bin/amixer -c 1 set Capture 80% || true
+      ${pkgs.alsa-utils}/bin/amixer -c 1 set 'Internal Mic Boost' 1 || true
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
     };
   };
 }
