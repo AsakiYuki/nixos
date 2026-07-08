1f6f6579adb6a784b88e0778ae613d1bc08db7e5
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-09 02:34:26 +0700

    ikd

--- users/junko/configuration.nix
+++ users/junko/configuration.nix
@@ -5,13 +5,11 @@
 }:
 libs.mkUsers config {
   junko = {
-    root = {
-      openssh.authorizedKeys.keys = libs.readRootFiles [
-        "/assets/public/ssh/asakiyuki/nixos/home-server.pub"
-        "/assets/public/ssh/asakiyuki/nixos/ideapad.pub"
-        "/assets/public/ssh/asakiyuki/nixos/ideapad.wsl.pub"
-        "/assets/public/ssh/junko/windows/hp-victus.pub"
-      ];
-    };
+    root.openssh.authorizedKeys.keys = libs.readRootFiles [
+      "/assets/public/ssh/asakiyuki/nixos/home-server.pub"
+      "/assets/public/ssh/asakiyuki/nixos/ideapad.pub"
+      "/assets/public/ssh/asakiyuki/nixos/ideapad.wsl.pub"
+      "/assets/public/ssh/junko/windows/hp-victus.pub"
+    ];
   };
 }
