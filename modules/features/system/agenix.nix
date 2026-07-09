563245b0b7e2db61ef7e27155ce9799e615960c8
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-09 15:58:53 +0700

    idk

--- modules/features/system/agenix.nix
+++ modules/features/system/agenix.nix
@@ -6,6 +6,6 @@
   ...
 }:
 lib.mkIf (inputs ? agenix) {
-  imports = [inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default];
+  environment.systemPackages = [inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default];
   programs.bash.interactiveShellInit = libs.readRootFile "/scripts/completions/agenix.sh";
 }
--- modules/features/system/waydroid.nix
+++ modules/features/system/waydroid.nix
@@ -6,16 +6,12 @@
   ...
 }: let
   cfg = config.virtualisation.waydroid;
-in
-  lib.mergeAttrs {
-    virtualisation.waydroid = {
-      enable = true;
-      package = pkgs.waydroid-nftables;
-    };
-  }
-  (lib.optionalAttrs cfg.enable {
-    programs.bash.interactiveShellInit = libs.readRootFile "/scripts/completions/waydroid.sh";
-    environment.systemPackages = cfg.enable [
-      pkgs.waydroid-helper
-    ];
-  })
+in {
+  virtualisation.waydroid = {
+    enable = true;
+    package = pkgs.waydroid-nftables;
+  };
+
+  programs.bash.interactiveShellInit = lib.mkIf cfg.enable (libs.readRootFile "/scripts/completions/waydroid.sh");
+  environment.systemPackages = lib.optionals cfg.enable [pkgs.waydroid-helper];
+}
