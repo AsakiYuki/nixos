64f068ef4ab4f67a01963618708bf7aa96d82119
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-09 17:03:51 +0700

    update password

--- secrets.nix
+++ secrets.nix
@@ -9,14 +9,17 @@ let
   system = {
     home-server = sshPubKey "home-server/system.nixos.pub";
     ideapad-slim-5-14apk10 = sshPubKey "ideapad-slim-5-14apk10/system.nixos.pub";
+    ideapad-slim-5-14apk10-wsl = sshPubKey "ideapad-slim-5-14apk10/system.nixos.wsl.pub";
   };
+
+  systems = [system.home-server system.ideapad-slim-5-14apk10 system.ideapad-slim-5-14apk10-wsl];
 in {
   "${svcPrefix}/cloudflare.secret.age".publicKeys = [system.home-server];
   "${svcPrefix}/nginx.auth.json.age".publicKeys = [system.home-server];
   "${svcPrefix}/searx.env.age".publicKeys = [system.home-server];
-  "${svcPrefix}/tailscale.secret.age".publicKeys = [system.home-server system.ideapad-slim-5-14apk10];
+  "${svcPrefix}/tailscale.secret.age".publicKeys = systems;
   "${usrPrefix}/asakiyuki.pwd.sha256.age" = {
-    publicKeys = [system.home-server system.ideapad-slim-5-14apk10];
+    publicKeys = systems;
     armor = true;
   };
 }
