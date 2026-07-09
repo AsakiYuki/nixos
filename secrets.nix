fcba9cf70c0a89e5dc2569b2d2906e63028f2fae
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-09 17:36:52 +0700

    refactor secrets.nix

--- secrets.nix
+++ secrets.nix
@@ -2,10 +2,6 @@ let
   fs = import ./libs/fileSystem.nix;
   sshPubKey = file: fs.readRootFile "/assets/public/ssh/${file}";
 
-  prefix = "assets/secrets";
-  svcPrefix = "${prefix}/services";
-  usrPrefix = "${prefix}/users";
-
   root = sshPubKey "root.key.pub";
   system = {
     home-server = sshPubKey "home-server/system.nixos.pub";
@@ -16,11 +12,11 @@ let
   systems = [system.home-server system.ideapad-slim-5-14apk10 system.ideapad-slim-5-14apk10-wsl];
 in
   builtins.mapAttrs (_: value: value // {publicKeys = value.publicKeys ++ [root];}) {
-    "${svcPrefix}/cloudflare.secret.age".publicKeys = [system.home-server];
-    "${svcPrefix}/nginx.auth.json.age".publicKeys = [system.home-server];
-    "${svcPrefix}/searx.env.age".publicKeys = [system.home-server];
-    "${svcPrefix}/tailscale.secret.age".publicKeys = systems;
-    "${usrPrefix}/asakiyuki.pwd.sha256.age" = {
+    "assets/secrets/services/cloudflare.secret.age".publicKeys = [system.home-server];
+    "assets/secrets/services/nginx.auth.json.age".publicKeys = [system.home-server];
+    "assets/secrets/services/searx.env.age".publicKeys = [system.home-server];
+    "assets/secrets/services/tailscale.secret.age".publicKeys = systems;
+    "assets/secrets/users/asakiyuki.pwd.sha256.age" = {
       publicKeys = systems;
       armor = true;
     };
