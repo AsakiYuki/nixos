ac3cbdf7db1c80258443839f55ab9e7357a1b73e
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-09 17:33:23 +0700

    add root.key.pub

--- secrets.nix
+++ secrets.nix
@@ -6,6 +6,7 @@ let
   svcPrefix = "${prefix}/services";
   usrPrefix = "${prefix}/users";
 
+  root = sshPubKey "root.key.pub";
   system = {
     home-server = sshPubKey "home-server/system.nixos.pub";
     ideapad-slim-5-14apk10 = sshPubKey "ideapad-slim-5-14apk10/system.nixos.pub";
@@ -13,13 +14,14 @@ let
   };
 
   systems = [system.home-server system.ideapad-slim-5-14apk10 system.ideapad-slim-5-14apk10-wsl];
-in {
-  "${svcPrefix}/cloudflare.secret.age".publicKeys = [system.home-server];
-  "${svcPrefix}/nginx.auth.json.age".publicKeys = [system.home-server];
-  "${svcPrefix}/searx.env.age".publicKeys = [system.home-server];
-  "${svcPrefix}/tailscale.secret.age".publicKeys = systems;
-  "${usrPrefix}/asakiyuki.pwd.sha256.age" = {
-    publicKeys = systems;
-    armor = true;
-  };
-}
+in
+  builtins.mapAttrs (_: keys: [root] ++ keys) {
+    "${svcPrefix}/cloudflare.secret.age".publicKeys = [system.home-server];
+    "${svcPrefix}/nginx.auth.json.age".publicKeys = [system.home-server];
+    "${svcPrefix}/searx.env.age".publicKeys = [system.home-server];
+    "${svcPrefix}/tailscale.secret.age".publicKeys = systems;
+    "${usrPrefix}/asakiyuki.pwd.sha256.age" = {
+      publicKeys = systems;
+      armor = true;
+    };
+  }
