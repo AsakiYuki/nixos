dad67d024041197afbbafc661b1668039b8ce731
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-09 12:19:31 +0700

    use screen to start server

--- devices/home-server/systemd/services/minecraft-bedrock.nix
+++ devices/home-server/systemd/services/minecraft-bedrock.nix
@@ -1,17 +1,20 @@
-{ ... }:
-
-{
+{pkgs, ...}: {
   systemd.services.minecraft-bedrock = {
     description = "Minecraft Bedrock Server";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
+    wantedBy = ["multi-user.target"];
+    after = ["network.target"];
 
     serviceConfig = {
-      Type = "simple";
+      Type = "forking";
       User = "asakiyuki";
       WorkingDirectory = "/home/asakiyuki/HOST/bedrockserver";
-      ExecStart = "/home/asakiyuki/HOST/bedrockserver/bedrock_server";
-      Environment = "LD_LIBRARY_PATH=.";
+      Environment = [
+        "LD_LIBRARY_PATH=."
+      ];
+
+      ExecStart = "${pkgs.screen}/bin/screen -dmS mc-bedrock ./bedrock_server";
+      ExecStop = "${pkgs.screen}/bin/screen -S mc-bedrock -X stuff 'stop^M'";
+
       Restart = "on-failure";
       RestartSec = "10s";
     };
