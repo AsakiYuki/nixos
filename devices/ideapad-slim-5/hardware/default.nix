6920bece3c331918c7a6d6fdec4fe03fcce50c7b
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-16 12:33:13 +0700

    idk

--- devices/ideapad-slim-5/hardware/default.nix
+++ devices/ideapad-slim-5/hardware/default.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   hardware = {
     enableAllFirmware = true;
     amdgpu.initrd.enable = true;
@@ -6,6 +6,14 @@
     graphics = {
       enable = true;
       enable32Bit = true;
+      extraPackages = with pkgs; [
+        libva-vdpau-driver
+        libvdpau-va-gl
+      ];
+      extraPackages32 = with pkgs.pkgsi686Linux; [
+        libva-vdpau-driver
+        libvdpau-va-gl
+      ];
     };
   };
 }
