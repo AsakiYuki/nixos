30ff1c83c327b87b1e3ebf1a6a90927ef5f04646
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-19 14:25:25 +0700

    syntax fix

--- modules/features/home/programs/cli/yt-dlp.nix
+++ modules/features/home/programs/cli/yt-dlp.nix
@@ -1,13 +1,12 @@
 {
   config,
-  lib,
+  libs,
   ...
 }: let
   cfg = config.programs.yt-dlp.output;
 in {
-  age.secrets.yt-dlp-cookies.file = lib.root "/assets/secrets/services/yt-dlp.cookies.age";
-
-  config.programs.yt-dlp = {
+  age.secrets.yt-dlp-cookies.file = libs.root "/assets/secrets/services/yt-dlp.cookies.age";
+  programs.yt-dlp = {
     extraConfig = ''
       -o ${config.home.homeDirectory}/${cfg.directory}/${cfg.format} --cookies ${config.age.secrets.yt-dlp-cookies.path}
     '';
