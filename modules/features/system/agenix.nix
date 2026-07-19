fa78d10c05f0769a64c344c2d8aa2a98d9cbb534
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-19 14:38:52 +0700

    refactor agenix secrets

--- modules/features/system/agenix.nix
+++ modules/features/system/agenix.nix
@@ -9,12 +9,16 @@
   age.secrets = let
     mkSecretIf = condition: file: (lib.mkIf condition {file = libs.root file;});
     cfgSvc = config.services;
+    hasUser = user: builtins.hasAttr user config.users.users;
   in {
+    # Service secrets
     cloudflare = mkSecretIf cfgSvc.cloudflare-dyndns.enable "/assets/secrets/services/cloudflare.secret.age";
     tailscale = mkSecretIf cfgSvc.tailscale.enable "/assets/secrets/services/tailscale.secret.age";
     nginxAuth = mkSecretIf cfgSvc.nginx.enable "/assets/secrets/services/nginx.auth.json.age";
     searxenv = mkSecretIf cfgSvc.searx.enable "/assets/secrets/services/searx.env.age";
-    asakiyukipwd.file = "/assets/secrets/users/asakiyuki.pwd.sha256.age";
+
+    # User passwords
+    asakiyukipwd = mkSecretIf (hasUser "asakiyuki") "/assets/secrets/users/asakiyuki.pwd.sha256.age";
   };
 
   environment.systemPackages = [inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default];
