62f8db7e9070762ae5c2dbaad8c26c25cc205331
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-22 21:48:39 +0700

    refactor cursors options

--- options/home/cursors.nix
+++ options/home/cursors.nix
@@ -1,24 +1,40 @@
 {
   lib,
+  libs,
   config,
-  custom,
   pkgs,
   ...
-}: {
+}: let
+  cursors = lib.importJSON (libs.root "/assets/cursors.json");
+  cursors-theme = config.theme.cursors;
+in {
   options = {
     theme.cursors = lib.mkOption {
-      type = lib.types.str;
+      type = lib.types.enum ([null] ++ (lib.attrNames cursors));
       default = "";
       description = "Cursor theme to use for the desktop";
     };
   };
 
-  config = lib.mkIf (config.theme.cursors != "") {
-    home.pointerCursor =
-      (custom.cursors {
-        name = config.theme.cursors;
-        size = 48;
-      })
-      {inherit pkgs;};
+  config.home.pointerCursor = lib.mkIf (cursors-theme != null) {
+    enable = true;
+    name = cursors-theme;
+    size = 48;
+    gtk.enable = true;
+    x11.enable = true;
+    hyprcursor.enable = true;
+    sway.enable = true;
+    dotIcons.enable = true;
+    package = let
+      url = "https://static.asakiyuki.com/packages/cursors/linux/${cursors-theme}.tar.gz";
+      hash = cursors.${cursors-theme};
+      packages = pkgs.fetchzip {
+        inherit url hash;
+      };
+    in (pkgs.runCommand "moveUp" {} ''
+      mkdir -p $out/share/icons
+      ln -s ${packages}
+      $out/share/icons/${cursors-theme}
+    '');
   };
 }
