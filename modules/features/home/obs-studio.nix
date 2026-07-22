177c7f7758e1d9f33241eff3cb5d165ad7ead3a5
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-22 22:13:34 +0700

    remove catppuccin obs from custom packages

--- modules/features/home/obs-studio.nix
+++ modules/features/home/obs-studio.nix
@@ -1,10 +1,12 @@
 {
   pkgs,
-  custom,
   lib,
   config,
   ...
 }:
 lib.mkIf config.programs.obs-studio.enable {
-  home.file.".config/obs-studio/themes".source = pkgs.callPackage custom.catppuccin-obs {};
+  home.file.".config/obs-studio/themes".source = "${pkgs.fetchzip {
+    url = "https://static.asakiyuki.com/packages/nixos/catppuccin-obs.zip";
+    sha256 = "sha256-Uk4a0HKaeyQilgBiPsuAWQubk1yZdyirNcfhYJEL+lQ=";
+  }}/themes";
 }
--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,5 @@
 {...}: {
   bun = import ./bun.nix;
   cage-xtmapper = {version ? "0.2.0"}: import ./cage-xtmapper-${version}.nix;
-  catppuccin-obs = import ./catppuccin-obs.nix;
   cider-2 = import ./cider-2.nix;
 }
