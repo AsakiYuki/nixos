b1d176c446dac485b5f1629a6e41e15b2075d5aa
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-22 19:10:08 +0700

    add fix-ssl command

--- devices/home-server/users/auth/asakiyuki.nix
+++ devices/home-server/users/auth/asakiyuki.nix
@@ -5,4 +5,11 @@
     "/ideapad-slim-5-14apk10/asakiyuki.nixos.wsl.pub"
     "/ideapad-slim-5-14apk10/asakiyuki.windows.pub"
   ] "/assets/public/ssh";
+
+  home-manager.users.asakiyuki.programs.bash.initExtra = ''
+    function fix-ssl() {
+      sudo systemctl restart "acme-$1.service"
+      sudo systemctl reload nginx
+    }
+  '';
 }
