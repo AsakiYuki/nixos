be07b577db79cf24d24bda29fc97dd10da0b1491
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-23 23:06:34 +0700

    uh

--- devices/home-server/systemd/services/minecraft-bedrock.nix
+++ devices/home-server/systemd/services/minecraft-bedrock.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{...}: {
   systemd.services.minecraft-bedrock = {
     description = "Minecraft Bedrock Server";
     wantedBy = ["multi-user.target"];
@@ -10,7 +10,7 @@
       Group = "mc-bedrock";
       WorkingDirectory = "/mnt/HOST/bedrockserver";
       Environment = [
-        "LD_LIBRARY_PATH=."
+        "LD_LIBRARY_PATH=/mnt/HOST/bedrockserver"
       ];
 
       ExecStart = "./bedrock_server";
