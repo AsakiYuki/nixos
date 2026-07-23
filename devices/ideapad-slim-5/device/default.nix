4f274a07bb75b3678137c20dc0e83fd293873a93
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-23 22:11:44 +0700

    add color profile

--- devices/ideapad-slim-5/device/default.nix
+++ devices/ideapad-slim-5/device/default.nix
@@ -1,7 +1,8 @@
-{...}: {
+{libs, ...}: {
   device.wm.hyprland.extraConfig = ''
     TouchpadName = "gxtp5100:00-27c6:01e0-touchpad"
 
+    local IccProfile = "${libs.root "/assets/colors/srgb_to_gamma2p2_400_mhc2.icm"}"
     ${builtins.readFile ./monitors.lua}
   '';
 }
--- devices/ideapad-slim-5/device/monitors.lua
+++ devices/ideapad-slim-5/device/monitors.lua
@@ -1,19 +1,20 @@
 -- monitors
-local isHDR = false
+local isHDR = true
 
-function updateHDR()
+local function updateHDR()
     hl.monitor({
         output = "eDP-1",
         mode = "1920x1200@60",
         position = "0x0",
         scale = 1,
         cm = isHDR and "hdr" or "dcip3",
+        sdr_eotf = "gamma22",
         bitdepth = 10,
         supports_wide_color = true,
         supports_hdr = true,
         sdr_min_luminance = 0.0,
         min_luminance = 0.0,
-        sdr_max_luminance = 150, 
+        sdr_max_luminance = 220, 
         max_luminance = 500,
         max_avg_luminance = 300,
     })
