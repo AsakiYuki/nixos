6d5f7418823a6909bd68848ba6f9eabfb6a2f406
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-23 23:14:14 +0700

    enable kde plasma

--- host/desktop/device.nix
+++ host/desktop/device.nix
@@ -7,7 +7,7 @@
     dm.sddm.enable = true;
     dm.gdm.enable = false;
 
-    de.kdePlasma.enable = false;
+    de.kdePlasma.enable = true;
     de.gnome.enable = false;
 
     wm.niri.enable = false;
