273a4d9e415a08f7a2b464c8cf16f03ffd7421e0
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-26 18:57:28 +0700

    uh

--- modules/system/programs/steam.nix
+++ modules/system/programs/steam.nix
@@ -1,13 +1,15 @@
 {
   pkgs,
   custom,
+  lib,
+  config,
   ...
 }: {
   programs.steam = {
     package = pkgs.millennium-steam;
   };
 
-  environment.systemPackages = [
+  environment.systemPackages = lib.mkIf config.programs.steam.enable [
     (pkgs.callPackage custom.wine-discord-ipc-bridge {})
   ];
 }
--- packages/wine-discord-ipc-bridge.nix
+++ packages/wine-discord-ipc-bridge.nix
@@ -10,21 +10,22 @@ stdenv.mkDerivation {
   src = fetchzip {
     url = "https://static.asakiyuki.com/packages/nixos/wine-discord-ipc-bridge.tar.gz";
     hash = "sha256-psh5fopcgq1cB+OGlqpDdeBMEOClNS40HzWACM6Ur5Q=";
+    stripRoot = false;
   };
 
   dontBuild = true;
 
   installPhase = ''
-    mkdir -p $out/bin/wine-discord-ipc
-    cp ./* $out/bin/wine-discord-ipc
-    chmod +x $out/bin/wine-discord-ipc/launch.sh
+    mkdir -p $out/bin
+    cp launch.sh $out/bin/wine-discord-ipc-bridge.sh
+    cp winediscordipcbridge.exe $out/bin/
+    chmod +x $out/bin/wine-discord-ipc-bridge.sh
   '';
 
   meta = with lib; {
-    description = "Enable games running under wine to use Discord Rich Presence";
+    description = "Enable games running under Wine to use Discord Rich Presence";
     homepage = "https://github.com/0e4ef622/wine-discord-ipc-bridge/";
-    license = with licenses; [
-      mit
-    ];
+    license = licenses.mit;
+    mainProgram = "wine-discord-ipc-bridge";
   };
 }
