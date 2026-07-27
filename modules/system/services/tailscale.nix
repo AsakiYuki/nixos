8da02c37b78e5a566e704c1e7008edb808eac62d
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-27 13:29:25 +0700

    change tailscale package version to unstable

--- modules/system/services/tailscale.nix
+++ modules/system/services/tailscale.nix
@@ -1,10 +1,12 @@
 {
   lib,
   config,
+  unstable,
   ...
 }:
 lib.mkIf config.services.tailscale.enable {
   services.tailscale = {
     authKeyFile = config.age.secrets.tailscale.path;
+    package = unstable.tailscale;
   };
 }
