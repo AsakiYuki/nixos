64cbe35e539faf497647554ab9d4451df7e5a3e7
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-27 17:24:43 +0700

    fix(nixpkgs) fix tailscale typo

--- overlays/tailscale.nix
+++ overlays/tailscale.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   nixpkgs.overlays = [
     (final: prev: {
-      tailcale = pkgs.callPackage (prev.tailscale.overrideAttrs (_: {
+      tailscale = pkgs.callPackage (prev.tailscale.overrideAttrs (_: {
         vendorHash = "sha256-Sd2iLJ7eDfDYdIRuW4xuiKgzhQWJWGAnz97FJWrVRlE=";
       })) {};
     })
