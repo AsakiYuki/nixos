153cdb19b18479e0b45f96886b309a617738b897
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-27 17:23:05 +0700

    fix(tailscale nixpkgs): vendorHash fail

--- overlays/tailscale.nix
+++ overlays/tailscale.nix
@@ -1,7 +1,9 @@
 {pkgs, ...}: {
   nixpkgs.overlays = [
     (final: prev: {
-      tailcale = pkgs.callPackage (prev.tailscale.overrideAttrs (prevAtts: rec {})) {};
+      tailcale = pkgs.callPackage (prev.tailscale.overrideAttrs (_: {
+        vendorHash = "sha256-Sd2iLJ7eDfDYdIRuW4xuiKgzhQWJWGAnz97FJWrVRlE=";
+      })) {};
     })
   ];
 }
