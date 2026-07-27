7ed5049b3ee636cdec59fea3275778ddb3162c39
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-27 17:28:11 +0700

    remove tailscale callPackages function

--- overlays/tailscale.nix
+++ overlays/tailscale.nix
@@ -1,9 +1,9 @@
-{pkgs, ...}: {
+{...}: {
   nixpkgs.overlays = [
     (final: prev: {
-      tailscale = pkgs.callPackage (prev.tailscale.overrideAttrs (_: {
+      tailscale = prev.tailscale.overrideAttrs (oldAttrs: {
         vendorHash = "sha256-Sd2iLJ7eDfDYdIRuW4xuiKgzhQWJWGAnz97FJWrVRlE=";
-      })) {};
+      });
     })
   ];
 }
