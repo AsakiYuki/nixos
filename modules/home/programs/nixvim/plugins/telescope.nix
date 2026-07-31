3968cab27ec4f685fb2696d74946025e0e3cc643
Asaki Yuki <vantrong2007vn@gmail.com>
2026-07-31 10:45:12 +0700

    fix(home-manager/nixvim) fix

--- modules/home/programs/nixvim/plugins/telescope.nix
+++ modules/home/programs/nixvim/plugins/telescope.nix
@@ -2,8 +2,8 @@
   enable = true;
   keymaps = {
     "<C-p>" = "git_files";
-    "<leader>fr" = "find_files";
-    "<leader>fg" = "oldfiles";
+    "<leader>ff" = "find_files";
+    "<leader>fr" = "oldfiles";
     "<leader>fg" = "live_grep";
     "<leader>fb" = "buffers";
     "<leader>fh" = "help_tags";
