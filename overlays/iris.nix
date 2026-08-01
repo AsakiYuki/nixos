d02efc5130eab63f15c42c7dd06b195562bb6c17
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-01 12:50:50 +0700

    add vendor hash

--- overlays/iris.nix
+++ overlays/iris.nix
@@ -5,7 +5,9 @@
 }: {
   nixpkgs.overlays = [
     (final: prev: {
-      iris = inputs.iris.packages.${pkgs.stdenv.hostPlatform.system}.default;
+      iris = inputs.iris.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (finalAttrs: prevAttrs: {
+        vendorHash = "sha256-KQNloP/Aj283YQ4d5LFu/2Pbb2HbVTZPhLK1fs4xvGw=";
+      });
     })
   ];
 }
