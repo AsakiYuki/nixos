841aa4933c52f172dffeffe488657792fad3cf2b
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-06 16:54:47 +0700

    ci(update.sh): change commit date format

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 nix flake update
 git add .
-git commit -m "chore(flake.lock): update flake inputs $(date +%d/%m/%y)"
+git commit -m "chore(flake.lock): update flake inputs $(date "+%H:%M:%S %d-%m-%Y")"
 git push origin HEAD
