3cb8e9d0d23732310bb75c1a30458bf4ba2d921b
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-07 17:50:05 +0700

    feat(git): add shortcut alias

--- modules/home/programs/cli/git.nix
+++ modules/home/programs/cli/git.nix
@@ -13,6 +13,9 @@
       };
 
       alias = {
+        ac = "!git add-commit ";
+        acp = "!git add-commit-push ";
+        fp = "!git fetch-pull";
         add-commit = "!git add . && git commit -m ";
         add-commit-push = "!fn() { git add-commit \"$1\"; git push origin HEAD; }; fn";
         fetch-pull = "!git fetch && git pull origin HEAD --rebase";
