9e99aab2d420b7c1b65a35c0c1281f30fea61229
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-07 18:27:09 +0700

    refactor: nixpkgs overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,17 @@
-{...}: {
-  imports = [
-    ./iris.nix
-    ./dolphin.nix
-    ./millennium.nix
-  ];
+{
+  inputs,
+  lib,
+  pkgs,
+  config,
+  ...
+}: let
+  iris-overlays = final: prev: {iris = inputs.iris.packages.${pkgs.stdenv.hostPlatform.system}.default;};
+in {
+  nixpkgs.overlays =
+    [
+      inputs.asa-overlay.overlays.default
+      inputs.millennium.overlays.default
+      iris-overlays
+    ]
+    ++ (lib.optional (!config.device.de.kdePlasma.enable) [inputs.dolphin-overlay.overlays.default]);
 }
