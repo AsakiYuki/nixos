0b3c1269500d3a79ff9cb735c2f60b71acea0279
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-07 10:23:02 +0700

    feat(git): add fetch-pull alias command

--- modules/home/programs/cli/git.nix
+++ modules/home/programs/cli/git.nix
@@ -15,6 +15,7 @@
       alias = {
         add-commit = "!git add . && git commit -m ";
         add-commit-push = "!fn() { git add-commit \"$1\"; git push origin HEAD; }; fn";
+        fetch-pull = "!git fetch && git pull origin HEAD --rebase";
       };
 
       # pull = {
