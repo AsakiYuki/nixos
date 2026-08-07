1ad26169bcf1c7934802340dce3e53a1ea99ca8f
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-07 10:23:59 +0700

    remove(scripts/bash): remove git utilities functions

--- scripts/bash.sh
+++ scripts/bash.sh
@@ -1,18 +1,3 @@
-function git-commit() {
-    git add .
-    git commit -m "$1"
-}
-
-function git-push() {
-    git-commit "$1"
-    git push origin HEAD
-}
-
-function git-pull() {
-    git fetch origin HEAD
-    git pull origin HEAD
-}
-
 function get-hash() {
     nix hash convert --hash-algo sha256 --from nix32 $(nix-prefetch-url --unpack "$1")
 }
