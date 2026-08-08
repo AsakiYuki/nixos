7f229f8ed091e1ec50e4fbaed6ed23a83773419c
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-08 23:09:27 +0700

    chore: ret boolean to check

--- scripts/check-package-updates.ts
+++ scripts/check-package-updates.ts
@@ -6,7 +6,7 @@ import { fetchZipHash } from "./helpers/nix"
 
 let packages: any = {}
 
-async function fetchLastBunBaselinePackage() {
+async function fetchLastBunBaselinePackage(): Promise<boolean> {
 	const packageName = "bun-baseline"
 	const cached = packages[packageName]
 
@@ -15,14 +15,14 @@ async function fetchLastBunBaselinePackage() {
 	const latestRelease = await getGithubRepoLatestRelease("oven-sh", "bun")
 	if (!latestRelease) {
 		console.warn(`[WARN] Could not fetch latest release for ${packageName}.`)
-		return null
+		return false
 	}
 
 	const newVersion = latestRelease.tag_name.split("v")[1]
 
 	if (cached && cached.version === newVersion) {
 		console.info(`[SKIP] ${packageName} is already up to date (v${newVersion}).`)
-		return
+		return false
 	}
 
 	console.info(`[UPDATE] New version found for ${packageName}: v${cached?.version || "none"} -> v${newVersion}`)
@@ -30,7 +30,7 @@ async function fetchLastBunBaselinePackage() {
 	const file = latestRelease.assets.find(({ name }) => name === "bun-linux-x64-baseline.zip")
 	if (!file) {
 		console.warn(`[WARN] Asset 'bun-linux-x64-baseline.zip' not found in release v${newVersion}.`)
-		return null
+		return false
 	}
 
 	console.info(`[INFO] Fetching zip hash from: ${file.download_url}`)
@@ -38,12 +38,16 @@ async function fetchLastBunBaselinePackage() {
 
 	packages[packageName] = { version: newVersion, hash }
 	console.info(`[SUCCESS] Updated ${packageName} to v${newVersion} with hash: ${hash}`)
+
+	return true
 }
 
 async function main() {
 	packages = JSON.parse(await fs.readFile(path.join(__dirname, "../assets/packages.json"), "utf-8"))
 
-	await Promise.all([fetchLastBunBaselinePackage()])
+	const status = await Promise.all([fetchLastBunBaselinePackage()])
+
+	if (!status.some(v => v)) return
 
 	await fs.writeFile(path.join(__dirname, "../assets/packages.json"), JSON.stringify(packages, null, 4))
 	// if (!test) return
